217080d24b3f536dd66b7fe61b7f91e1bfc7a7d0
Ygreneb <ygreneb@benson.uber.space>
2021-07-07 16:43:18 +0200

    Commit of helloworld example files, GUID and names already updated

---
+++ include/plugcontroller.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "public.sdk/source/vst/vsteditcontroller.h"
+#include "vstgui/plugin-bindings/vst3editor.h"
+
+namespace Steinberg {
+namespace HelloWorld {
+
+//-----------------------------------------------------------------------------
+class PlugController : public Vst::EditController, public VSTGUI::VST3EditorDelegate
+{
+public:
+//------------------------------------------------------------------------
+	// create function required for Plug-in factory,
+	// it will be called to create new instances of this controller
+//------------------------------------------------------------------------
+	static FUnknown* createInstance (void*)
+	{
+		return (Vst::IEditController*)new PlugController ();
+	}
+
+	//---from IPluginBase--------
+	tresult PLUGIN_API initialize (FUnknown* context) SMTG_OVERRIDE;
+
+	//---from EditController-----
+	IPlugView* PLUGIN_API createView (const char* name) SMTG_OVERRIDE;
+	tresult PLUGIN_API setComponentState (IBStream* state) SMTG_OVERRIDE;
+};
+
+//------------------------------------------------------------------------
+} // namespace HelloWorld
+} // namespace Steinberg
---
+++ include/plugids.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace Steinberg {
+namespace HelloWorld {
+
+// HERE are defined the parameter Ids which are exported to the host
+enum HelloWorldParams : Vst::ParamID
+{
+	kBypassId = 100,
+
+	kParamVolId = 102,
+	kParamOnId = 1000
+};
+
+
+// HERE you have to define new unique class ids: for processor and for controller
+// you can use GUID creator tools like https://www.guidgenerator.com/
+static const FUID MyProcessorUID (0x51A7E9BA, 0x82DE4F6E, 0xB64B6384, 0xEE075ED2);
+static const FUID MyControllerUID (0x143C25B4, 0xD9C14CDD, 0x9D534026, 0xE73EF071);
+
+
+//------------------------------------------------------------------------
+} // namespace HelloWorld
+} // namespace Steinberg
